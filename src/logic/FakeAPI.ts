45db9c27197c7e67a017297c91758e0d05b2d589
redstone59 <redstonepro23@googlemail.com>
2024-07-21 21:49:14 +1000

    add dlLevel func to fake api

--- src/logic/FakeAPI.ts
+++ src/logic/FakeAPI.ts
@@ -1,7 +1,11 @@
-import { APIInformation } from "./NineCirclesLevel"
+import { APIInformation } from "./NineCirclesLevel";
+import { downloadedAPIInfo } from "./FakeAPIInformation";
 
 function dlLevel(levelId: number): APIInformation {
-    
+    if (levelId.toString() in Object.keys(downloadedAPIInfo)) {
+        return downloadedAPIInfo[levelId.toString()];
+    }
+    throw new RangeError("Level ID not present within downloaded data.")
 }
 
 export { dlLevel }
