3beb1a935d449031131588ed758c9ac30ba44ac9
redstone59 <redstonepro23@googlemail.com>
2024-07-23 15:31:46 +1000

    remove 'in' because that didn't work

--- src/logic/FakeAPI.ts
+++ src/logic/FakeAPI.ts
@@ -2,7 +2,7 @@ import { APIInformation } from "./NineCirclesLevel";
 import { downloadedAPIInfo } from "./FakeAPIInformation";
 
 function dlLevel(levelId: number): APIInformation {
-    if (levelId.toString() in Object.keys(downloadedAPIInfo)) {
+    if (Object.keys(downloadedAPIInfo).includes(levelId.toString())) {
         return downloadedAPIInfo[levelId.toString()];
     }
     throw new RangeError("Level ID not present within downloaded data.")
