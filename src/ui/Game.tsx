8cec0689c9ccee8b7461fb8890b1aa411fd44324
yanqui <20507951+yanqui-uxo@users.noreply.github.com>
2024-08-17 23:10:48 -0700

    Clunky combobox

--- src/ui/Game.tsx
+++ src/ui/Game.tsx
@@ -1,42 +1,87 @@
-import { allLevels, nameToIdObj } from "../logic/AllLevels";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
 
 import { FullLevelInfo } from "../logic/NineCirclesLevel";
 import { GuessResult } from "../logic/Guess";
 import GuessResultView from "./GuessResultView";
 import { NineCircle } from "../logic/NineCircle";
+import { allLevels } from "../logic/AllLevels";
 import { downloadedAPIInfo } from "../logic/FakeAPIInformation";
+import { idToReadable } from "../logic/ReadableLevel";
 import { useState } from "react";
 
 const game = new NineCircle();
 
 export default function Game() {
-  const [nameGuess, setNameGuess] = useState("");
-  const [guesses, setGuesses] = useState<[FullLevelInfo, GuessResult][]>([]);
+  const [nameQuery, setNameQuery] = useState("");
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [guesses, setGuesses] = useState<[string, GuessResult][]>([]);
+
+  const fullLevelInfos: { [id: string]: FullLevelInfo } = Object.fromEntries(
+    Object.keys(allLevels).map((id) => [
+      id,
+      { ...allLevels[id], ...downloadedAPIInfo[id] },
+    ]),
+  );
+  const levelIds = Object.keys(allLevels);
+  const filteredLevelEntries: [string, FullLevelInfo][] =
+    nameQuery === ""
+      ? []
+      : levelIds
+          .filter((id) =>
+            downloadedAPIInfo[id].name
+              .toLowerCase()
+              .startsWith(nameQuery.toLowerCase()),
+          )
+          .map((id) => [id, fullLevelInfos[id]]);
+  const filteredLevels = Object.fromEntries(filteredLevelEntries);
+  console.log(filteredLevels);
+
+  function submit() {
+    (async () => {
+      if (!selectedId || guesses.some((g) => g[0] === selectedId)) {
+        return;
+      }
+      const guessResult = await game.guessReadableLevel(
+        idToReadable[selectedId],
+      );
+      setGuesses([[selectedId, guessResult], ...guesses]);
+    })();
+  }
 
   return (
     <div className="flex flex-col items-center">
       <form
-        onSubmit={async (e) => {
+        onSubmit={(e) => {
           e.preventDefault();
-          const levelId = nameToIdObj[nameGuess].toString();
-          const levelInfo: FullLevelInfo = {
-            ...downloadedAPIInfo[levelId],
-            ...allLevels[levelId],
-          };
-          const guessResult = await game.guessLevel(nameGuess);
-          setGuesses([[levelInfo, guessResult], ...guesses]);
+          submit();
         }}
       >
-        <input
-          className="border border-black"
-          name="guess"
-          value={nameGuess}
-          onChange={(e) => setNameGuess(e.target.value)}
-        ></input>
+        <Combobox value={selectedId} onChange={setSelectedId}>
+          <ComboboxInput<string>
+            onChange={(e) => setNameQuery(e.target.value)}
+            displayValue={(id) => filteredLevels[id]?.name}
+          />
+          <ComboboxOptions className="empty:invisible">
+            {filteredLevelEntries.map(([id, l]) => (
+              <ComboboxOption
+                key={id}
+                value={id}
+                className="data-[focus]:bg-red-100"
+              >
+                {l.name} ({l.creators.join(", ")})
+              </ComboboxOption>
+            ))}
+          </ComboboxOptions>
+        </Combobox>
         <button type="submit">Submit</button>
       </form>
       {guesses.map((g) => (
-        <GuessResultView info={g[0]} result={g[1]} />
+        <GuessResultView key={g[0]} info={fullLevelInfos[g[0]]} result={g[1]} />
       ))}
     </div>
   );
